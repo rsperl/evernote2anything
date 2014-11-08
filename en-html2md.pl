3ef4b0b7ab7dfc0bd5da623ca1683872580caafe
Richard Sugg <richard.sugg@sas.com>
2014-11-08 12:26:37 -0500

    removed unnecessary open

--- en-html2md.pl
+++ en-html2md.pl
@@ -16,10 +16,6 @@ my $DEBUG = ($ENV{DEBUG} && $ENV{DEBUG} == 1? 1 : 0);
 my $input_file = shift @ARGV;
 chomp $input_file;
 
-open my $fh, '<', $input_file;
-my $html = do { local $/; <$fh>; };
-close $fh;
-$html =~ s/<a name="\d+?"\/>//;
 try {
     my $output = EN->new(html_filename => $input_file)
                 ->parse
