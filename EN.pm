176ee3411ba8ab68ca2d0be32becc971e71592e3
Richard Sugg <richard.sugg@sas.com>
2014-11-08 12:24:21 -0500

    mostly working for notes without images

--- EN.pm
+++ EN.pm
@@ -4,13 +4,14 @@ use strict;
 use Moose;
 use Mojo::DOM;
 use DateTime;
-use HTML::WikiConverter::Markdown;
+use HTML::WikiConverter;
 use Data::Dumper;
 
 
-has input_html => (is => 'ro', isa => 'Str',      required     => 1);
+has html_filename => (is => 'ro', isa => 'Str', required => 1);
+has input_html => (is => 'rw', isa => 'Str',      required     => 0);
 
-has tags       => (is => 'rw', isa => 'Str', default => '');
+has tags        => (is => 'rw', isa => 'ArrayRef', default => sub {[]});
 #has created    => (is => 'rw', isa => 'DateTime', default => sub { DateTime->now });
 has created    => (is => 'rw', isa => 'Str', default => '');
 has author     => (is => 'rw', isa => 'Str', default => '');
@@ -22,14 +23,56 @@ has location   => (is => 'rw', isa => 'Str', default => '');
 has body       => (is => 'rw', isa => 'Str', default => '');
 
 my $html = Mojo::DOM->new;
-my $wc   = HTML::WikiConverter::Markdown->new(dialect => "Markdown");
+my $wc   = HTML::WikiConverter->new(
+                dialect    => "Markdown",
+                link_style => "inline",
+                image_style => "inline",
+                md_extra   => 1,
+            );
 
 sub BUILD {
     my ($self, $opts) = @_;
+    open my $fh, '<', $opts->{html_filename} or die "Can't open $opts->{html_filename}: $!";
+    $opts->{input_html} = do { local $/; <$fh>; };
+    $opts->{input_html} =~ s/<a name="\d+?"\/>//;
+    $self->input_html( $opts->{input_html} );
+    close $fh;
+    if(
+        index($opts->{input_html}, "<img") != -1
+    ) {
+        #die "$opts->{html_filename} contains images"
+    }
+}
+
+sub save {
+    my ($self) = @_;
+    my $format =<<'EOF';
+# %s
+
+| **Created:** | %s |
+| **Updated:** | %s |
+| **Author:**  | %s |
+| **Tags:**    | %s |
+| **Source:**  | %s |
+
+%s
+EOF
+    my $filename = $self->html_filename;
+    $filename =~ s/\.html$/.md/;
+    open my $fh, '>', $filename or die "Can't open $filename for writing: $!";
+    print $fh sprintf $format,
+        $self->title,
+        $self->created, $self->updated, $self->author,
+        join(", ", @{ $self->tags }),
+        $self->source,
+        $self->body;
+    close $fh;
+    return $filename;
 }
 
 sub parse {
     my ($self) = @_;
+
     my $dom = $html->parse($self->input_html);
     my $title = $dom->find("html title")->[0]->text;
     $self->title($title);
@@ -42,7 +85,21 @@ sub parse {
             my $name = lc($row_dom->find("b")->[0]->text);
             $name =~ s/:$//;
             my $value = $row_dom->find("i")->[0]->text;
-            $self->$name($value);
+            if( $name =~ /tags/i ) {
+                my @a = split(/,/, $value);
+                for(my $i=0; $i<@a; $i++) {
+                    $a[$i] =~ s/^\s+//;
+                    $a[$i] =~ s/\s+$//;
+                    $a[$i] =~ s/\s+/_/g;
+                }
+                $self->$name(\@a);
+            } else {
+                $self->$name($value);
+            }
+
         }
     );
+
+    $self->body($wc->html2wiki($self->input_html));
+    return $self;
 }
--- en-html2md.pl
+++ en-html2md.pl
@@ -4,112 +4,28 @@ use strict;
 use warnings;
 use FindBin;
 use Try::Tiny;
-use XML::Simple;
 use Data::Dumper;
 use Data::Printer;
 use DateTime;
-use HTML::WikiConverter::Markdown;
 use Getopt::Long;
+use EN;
 use utf8;
 
 my $DEBUG = ($ENV{DEBUG} && $ENV{DEBUG} == 1? 1 : 0);
 
-my @DO_NOT_CONVERT_IF_TAGGED = qw(markdown);
+my $input_file = shift @ARGV;
+chomp $input_file;
 
-my ($IMPORT_FILE, $OUTPUT_DIR);
-GetOptions(
-    "import:s" => \$IMPORT_FILE,
-    "output-dir:s" => \$OUTPUT_DIR,
-);
-
-if( ! ($IMPORT_FILE && $OUTPUT_DIR) ) {
-    print "Usage: $FindBin::Script --import <file.html> --output-dir <dir>\n";
-    exit 1;
-}
-
-my $wc = HTML::WikiConverter->new(dialect => "Markdown");
-open my $fh, '<', $IMPORT_FILE;
-my $content = do { local $/; <$fh>; };
+open my $fh, '<', $input_file;
+my $html = do { local $/; <$fh>; };
 close $fh;
-if( index($content, "<en-media") >= 0 ||
-    index($content, "<en-todo")  >= 0 ) {
-    $content = $wc->html2wiki($content);
-open $fh, '>', "test_html.md";
-print $fh $content;
-close $fh;
-print "finished\n";
-
-__END__
-
-foreach my $note ( @notes ) {
-    print Dumper($note);
-    exit;
-    my $parsed_note = parse_note($note);
-    save_note($parsed_note);
-}
-
-sub save_note {
-    my $note = shift;
-    print "start tags: " . Dumper($note->{tags}) . "\n";
-    my @tags = sort
-                    @{$note->{tags}},
-                    $note->{created}->ymd("") . '-' . $note->{created}->hms("");;
-    for(my $i=0; $i<@tags; $i++) {
-        $tags[$i] =~ s/\s+/_/g;
-    }
-    my $tags = join(' ', @tags);
-    my $filename = $note->{title} . "[$tags].md";
-    print "filename: $filename\n";
-    open my $fh , '>', "$OUTPUT_DIR/$filename";
-    print $fh $note->{content};
-    close $fh;
-}
-
-sub parse_note {
-    my $note = shift;
-    my $title = $note->{title};
-    my @tags;
-    if( exists $note->{tag} ) {
-        if( ref($note->{tag}) eq 'ARRAY' ) {
-            @tags = @{$note->{tag}};
-        } elsif( ref($note->{tag}) eq '' ) {
-            @tags = ( $note->{tag} );
-        }
-    }
-    my $content = "";
-    try {
-        if( ! array_contains(\@DO_NOT_CONVERT_IF_TAGGED, \@tags) ) {
-            $content = $wc->html2wiki($note->{content});
-        }
-        $content =~ s/\\x{(.+?)}/chr(hex($1))/ge;
-        # $content =~ s/(.)/(ord($1) > 127) ? "" : $1/egs;
-    } catch {
-        $content = $_;
-    };
-    my $dt_created;
-    if( $note->{created} =~ /^(\d\d\d\d)(\d\d)(\d\d)T(\d\d)(\d\d)(\d\d)Z$/ ) {
-        $dt_created = DateTime->new(
-                            time_zone => "UTC", year => $1, month => $2, day => $3,
-                            hour => $4, minute => $5, second => $6
-                        );
-    } else {
-        print "bad created time: $note->{created}\n";
-    }
-    return {
-        title   => $title,
-        tags    => \@tags,
-        content => $content,
-        created => $dt_created,
-    };
-}
-
-sub array_contains {
-    my ($needles, $haystack) = @_;
-    die "needles must be an arrayref"  unless ref($needles)  eq 'ARRAY';
-    die "haystack must be an arrayref" unless ref($haystack) eq 'ARRAY';
-    foreach my $needle (@$needles) {
-        return 1 if grep({/^$needle$/} @$haystack);
-    }
-    return 0;
-}
+$html =~ s/<a name="\d+?"\/>//;
+try {
+    my $output = EN->new(html_filename => $input_file)
+                ->parse
+                ->save;
+    print "Saved $output\n";
+} catch {
+    print $_;
+};
 
